8051443651cf3453589bf389fb89a54271d19057
Bteven <smorga300@caledonian.ac.uk>
2025-02-11 02:18:53 +0000

    Enemy pathing complete

--- Assets/Scripts/EnemyPath.cs
+++ Assets/Scripts/EnemyPath.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.U2D;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class EnemyPath : MonoBehaviour
 {
     [SerializeField]
     private Transform pointA, pointB, pointC, pointD; //control points;
@@ -22,11 +23,6 @@ public class Enemy : MonoBehaviour
 
     public float speed;
 
-
-
-  
-
-
     void Start()
     {
         tcount = 0f;
@@ -37,27 +33,24 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         if (coroutineAllowed)
         {
             StartCoroutine(EnemyRoute(routeToGo));
         }
-
     }
 
     private void OnDrawGizmos() //draw enemy path in viewport
     {
-       for(float t = 0; t < 1; t += 0.05f)
+       for(float t = 0; t < 1; t += 0.02f)
         {
             gizmosPos = Mathf.Pow(1 - t, 3) * pointA.position + 3 * Mathf.Pow(1 - t, 2) * t * pointB.position +
                 3 * Mathf.Pow(1 - t, 1) * Mathf.Pow(t, 2) * pointC.position + Mathf.Pow(t, 3) * pointD.position;
 
             Gizmos.color = Color.green;
 
-            Gizmos.DrawSphere(gizmosPos, 0.1f);
+            Gizmos.DrawSphere(gizmosPos, 0.2f);
 
             //generates a visual representation of the curve
-
         }
 
         Gizmos.DrawLine(pointA.position, pointB.position);
@@ -74,30 +67,25 @@ public class Enemy : MonoBehaviour
         Vector3 p2 = routes[routeNumber].GetChild(2).position;
         Vector3 p3 = routes[routeNumber].GetChild(3).position; //gets the position of each point in the routes array
 
-        for (tcount = 0; tcount < 1; tcount += speed) // t=0 is the start of the curve and t=1 is the end. object moves along curve according to the speed
+        for (tcount = 0; tcount < 1; tcount += 0.0005f) 
         {
-
             enemyPos = Mathf.Pow(1 - tcount, 3) * p0 + 3 * Mathf.Pow(1 - tcount, 2) * tcount * p1 +
                 3 * Mathf.Pow(1 - tcount, 1) * Mathf.Pow(tcount, 2) * p2 + Mathf.Pow(tcount, 3) * p3;
 
-
-
-            transform.position = enemyPos;
+            transform.position = Vector3.MoveTowards(transform.position, enemyPos,speed *  Time.deltaTime);
             yield return new WaitForEndOfFrame();
+        }
+     
+        tcount = 0f;
+        routeToGo += 1; // after the route is finished it goes to the next
 
-            tcount = 0f;
-            routeToGo += 1; // after the route is finished it goes to the next
-
-            if (routeToGo > routes.Length - 1) //if there is no next route it goes back to the first
-            {
-                routeToGo = 0;
-            }
-
-
-
-            coroutineAllowed = true; //coroutine repeats
-
+        if (routeToGo > routes.Length - 1) //if there is no next route it goes back to the first
+        {
+            routeToGo = 0;
         }
 
+        coroutineAllowed = true; //coroutine repeats
+
+        
     }
 }
