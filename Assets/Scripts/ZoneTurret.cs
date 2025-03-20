28d593129f698108529ce03e5f39e8a091e446b7
Roddy <rmacmi300@caledonian.ac.uk>
2025-03-20 13:39:55 +0000

    UI and Zone Turret

--- Assets/Scripts/ZoneTurret.cs
+++ Assets/Scripts/ZoneTurret.cs
@@ -1,10 +1,21 @@
-using System.Collections;
+ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ZoneTurret : BaseTurret
 {
-    // Start is called before the first frame update
+
+
+    public Animator animator;
+    [SerializeField] float damageAmount;
+
+
+    [Header("Firing Variables")]
+
+    public float fireCooldown;
+    public float currentFireTimer;
+
+
     void Start()
     {
 
@@ -13,34 +24,46 @@ public class ZoneTurret : BaseTurret
     // Update is called once per frame
     void Update()
     {
-
+        base.Update();
     }
     protected override void TurretRotation()       //overrides base class so it is only calls the turret to attack and not rotate as it dosn't need to
     {
 
-        if (enemyObject != null) // checks of enemy is there before finding direction and rotating towards
+        if (enemyObject != null)
         {
-
-            SpinAttack();
-
+            firingSequence();
         }
+
+
+    }
+
+    void firingSequence()           // copyed from firing tower
+    {
+
+     
+            currentFireTimer += Time.deltaTime;      // adds to the timer
+
+            if (currentFireTimer > fireCooldown)        // when cooldown over
+            {
+                SpinAttack();                  // calls methoud to fire the bullet
+                currentFireTimer = 0;       // resets timer
+            }
+        
     }
 
     void SpinAttack()
     {
 
+       
 
-        foreach (GameObject enemy in enemyObjectInRange)
-        {
-            if (enemy != null)
-            { 
+            animator.SetTrigger("Attacking");
             
-          
-            
+            foreach (GameObject enemy in enemyObjectInRange)
+            {
+
+            HealthController enemyHealth = enemy.GetComponent<HealthController>();
+                enemyHealth.TakeDamage(damageAmount);
+
             }
-      
-        }
-
-
     }
 }
