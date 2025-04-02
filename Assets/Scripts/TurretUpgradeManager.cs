e8dc4213cc8f5cc6d39f8aead28803bb6c0d0176
Bteven <smorga300@caledonian.ac.uk>
2025-04-02 14:17:50 +0100

    cursorChange edited

--- Assets/Scripts/TurretUpgradeManager.cs
+++ Assets/Scripts/TurretUpgradeManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TurretUpgradeManager : MonoBehaviour
 {
@@ -12,8 +13,8 @@ public class TurretUpgradeManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
         turretIsSelected = false;
+
 
     }
 
@@ -64,6 +65,5 @@ public class TurretUpgradeManager : MonoBehaviour
 
 
         }
-
     }
 }
--- Assets/Scripts/cursorChange.cs
+++ Assets/Scripts/cursorChange.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,15 +10,19 @@ public class cursorChange : MonoBehaviour
 
     public Button btn;
     public Texture2D upgradeCursor;
-    void Start()
+    private TurretUpgradeManager TUManager;
+
+    private void Start()
     {
-        
+        TUManager = FindAnyObjectByType<TurretUpgradeManager>();
+        TUManager.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         btn.onClick.AddListener(ChangeCursor);
+        TUManager.enabled = true;
     }
 
     void ChangeCursor()
